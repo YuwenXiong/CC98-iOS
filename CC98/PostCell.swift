02a87f92a5f4db909a32b3ebc5403ee1597d73a1
zax <zhongaoxiao@gmail.com>
2015-12-14 20:35:47 +0800

    1

--- CC98/PostCell.swift
+++ CC98/PostCell.swift
@@ -24,8 +24,8 @@ class PostCell:UITableViewCell,UIWebViewDelegate{
         let width = webView.stringByEvaluatingJavaScriptFromString("document.body.offsetWidth")
         if height != "" {
             var frame=self.frame
-            frame.size.height = CGFloat((height! as NSString).doubleValue)
-            frame.size.width = CGFloat((width! as NSString).doubleValue)
+            frame.size.height = CGFloat((height! as NSString).doubleValue)+20
+            frame.size.width = CGFloat((width! as NSString).doubleValue)+20
 //            frame.size.width
             self.frame = frame
 //            webView.frame=frame
@@ -35,7 +35,7 @@ class PostCell:UITableViewCell,UIWebViewDelegate{
     func setView(){
         let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
         webView.loadHTMLString(content!, baseURL: baseURL)
-        webView.scrollView.bounces = false
+        webView.scrollView.scrollEnabled = false
         webView.layer.cornerRadius = 6;
         webView.layer.masksToBounds = true
 //        let height = webView.stringByEvaluatingJavaScriptFromString("document.bo")
