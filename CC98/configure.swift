3e5bd92676779dae67d16f77581571eb9497f669
Orpine <Orpine@msn.com>
2015-12-17 21:37:42 +0800

    update configure

--- CC98/configure.swift
+++ CC98/configure.swift
@@ -17,13 +17,15 @@ let globalDataProcessor = DataProcessor()
 
 
 
-//let settings = [
-//    "client_id": "CC98",
-//    "client_secret": "84e21510-02f4-4992-b6ee-823a3f64e33d",
-//    "authorize_uri": "https://login.cc98.org/OAuth/Authorize",
-//    "token_uri": "https://login.cc98.org/OAuth/Token",
-//    "scope": "profile email",
-//    "redirect_uris": ["cc98://oauth/callback"],     // don't forget to register this scheme
-//    "keychain": false,                              // if you DON'T want keychain integration
-//    "title": "CC98 OAuth Login"                     // optional title to show in views
-//    ] as OAuth2JSON                                 // the "as" part may or may not be needed
+let settings = [
+    "client_id": "a9f98862-05ad-417f-814b-81c05804a047",
+    "client_secret": "84e21510-02f4-4992-b6ee-823a3f64e33d",
+    "authorize_uri": "https://login.cc98.org/OAuth/Authorize",
+    "token_uri": "https://login.cc98.org/OAuth/Token",
+    "scope": "all",
+    "redirect_uris": ["cc98://oauth/callback"],     // don't forget to register this scheme
+    "keychain": true,                              // if you DON'T want keychain integration
+    "title": "CC98 OAuth Login"                     // optional title to show in views
+    ] as OAuth2JSON                                 // the "as" part may or may not be needed
+
+let oauth = OAuthrCodeGrant(settings: settings)
